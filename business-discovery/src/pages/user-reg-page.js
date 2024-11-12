1758083c370d36483421ab55ca0a58d8b606bd35
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-12 18:36:01 +0200

    Start

---
+++ business-discovery/src/pages/user-reg-page.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const register = () =>{
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        emailAdress: '',
+        username:'',
+        password:'',
+        confirmPassword:''
+    });
+
+
+}
