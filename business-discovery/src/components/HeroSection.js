10886bcf541ecb4eef9a5f801bee1702060d9ab1
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-13 16:10:15 +0200

    Styling

--- business-discovery/src/components/HeroSection.js
+++ business-discovery/src/components/HeroSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styling/mystyling.scss';
 
 const HeroSection = () => {
     return(
         <section className='hero'>
             <div>
     /**Add a link to discover local business as a user */   /**--- */   /** Business sign-up here*/
-                <h1>Discover local business around you or set up your business to be found by locals and tourists</h1>
+                <h1 className='my-sass'>Discover local business around you or set up your business to be found by locals and tourists</h1>
             </div>
         </section>
     );
