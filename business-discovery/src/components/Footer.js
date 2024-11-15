509939f73a166803928ce69c56f70384c81e8a71
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-15 11:59:21 +0200

    Footer updated and styled. Small styling for features

--- business-discovery/src/components/Footer.js
+++ business-discovery/src/components/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import '../styling/Footer.scss';
 
 const Footer = () => {
     return(
-        <footer>
-            <p>All Rights Reserved</p>
-        </footer>
+        <div className='footer'>
+            <div className='footer-content'>
+                <p>All Rights Reserved &copy; 2024</p>
+                <ul className='footer-links'>
+                    <li><a href='#Privacy'>Privacy Policy</a></li>
+                    <li><a href='#TOS'>Terms of Service</a></li>
+                    <li><a href='#Contact'>Contact</a></li>
+                </ul>
+                <p className='footer-social'>
+                    Follow us:
+                    <a href='#twitter' target='_blank'>Twitter</a>
+                    <a href='#facebook' target='_blank'>Facebook</a>
+                    <a href='#instagram' target='_blank'>Instagram</a>
+                </p>
+            </div>
+        </div>
     );
 };
 
