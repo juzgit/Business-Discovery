e8348abadf129b036dbcc0f70c66d81d1bb96954
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-15 11:30:45 +0200

    Updated JoinSection and styled it.

--- business-discovery/src/components/JoinSection.js
+++ business-discovery/src/components/JoinSection.js
@@ -3,13 +3,13 @@ import '../styling/JoinSection.scss';
 
 const JoinSection = () => {
     return(
-        <div>
-            <h2>Ready to Join?</h2>
-            <p>Sign up today and start discovering or sharing your local business!</p>
+        <div className="join-section" >
+            <h2 className="join-section__title">Ready to Join?</h2>
+            <p className='join-section__description'>Sign up today and start discovering or sharing your local business!</p>
 
-            <div>
-                <a href='#home'>Sign Up as User</a>
-                <a href='#home'>Sign Up as Business</a>
+            <div className='join-section__buttons'>
+                <a href='#home' className='join-section__link'>Sign Up as User</a>
+                <a href='#home' className='join-section__link'>Sign Up as Business</a>
             </div>
         </div>
     );
