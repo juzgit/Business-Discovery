579c93a5d6f5bdcb88ad53e288c85765e79992ce
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-15 10:17:12 +0200

    Updated FeatureSection and styled it

--- business-discovery/src/components/FeatureSection.js
+++ business-discovery/src/components/FeatureSection.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import "../styling/FeatureSectionStyling.scss";
 
 const FeaturesSection = () => {
     return (
-        <section>
-            <h2>Key Features</h2>
+        <div className="key-features">
+            <h2 className='features-section__title'>Key Features</h2>
 
             
-            <div>
-                <div>
-                    <h3>
+            <div className='features-section__content'>
+                <div className='features-section__block features-section__block--user'>
+                    <h3 className='features-section__block-title'>
                         User Features
                     </h3>
 
-                    <ul>
-                        <li>Discover local businesses by category, location, and keyword.</li>
-                        <li>Leave reviews and ratings to help others find the best spots.</li>
-                        <li>Get perosnalised recommendations based on your interests.</li>
+                    <ul className='features-section__list'>
+                        <li className='features-section__item'>Discover local businesses by category, location, and keyword.</li>
+                        <li className='features-section__item'>Leave reviews and ratings to help others find the best spots.</li>
+                        <li className='features-section__item'>Get perosnalised recommendations based on your interests.</li>
                     </ul>
                 </div>
 
-                <div>
-                    <h3>Business Features</h3>
-                    <ul>
-                        <li>Create a profile and showcase your business.</li>
-                        <li>Promote offers, discounts, and events to attract.</li>
-                        <li>Manage customer reviews and track your performance with analytics.</li>
+                <div className='features-section__block features-section__block--business'>
+                    <h3 className='features-section__block-title'>Business Features</h3>
+                    <ul className='features-section__list'>
+                        <li className='features-section__item'>Create a profile and showcase your business.</li>
+                        <li className='features-section__item'>Promote offers, discounts, and events to attract.</li>
+                        <li className='features-section__item'>Manage customer reviews and track your performance with analytics.</li>
                     </ul>
                 </div>
+            
             </div>
-        </section>
+        </div>
 
     );
 };
