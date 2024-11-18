01eaaae25ae7acf9b084d4c0d055ee2a2d6449e0
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-18 16:46:50 +0200

    Updated and styled business-reg-page

--- business-discovery/src/App.js
+++ business-discovery/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //import Homepage from "./pages/HomePage";
 //import UserRegister from "./pages/user-reg-page";
-import UserLogin from "./pages/user-login-page";
+//import UserLogin from "./pages/user-login-page";
+import BusinessRegister from "./pages/business-reg-page";
 
 function App() {
   return (
     <div className="App">
         <Router>
             <Routes>
-            <Route path="/" element={<UserLogin />} />
+            <Route path="/" element={<BusinessRegister />} />
             </Routes>
         </Router>
     </div>
--- business-discovery/src/pages/business-reg-page.js
+++ business-discovery/src/pages/business-reg-page.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import '../pagesStyling/business-reg.scss'
 
 const BusinessRegister = () => {
     const [formData, setFormData] = useState({
@@ -18,11 +19,11 @@ const BusinessRegister = () => {
     }
 
     const validate = () =>{
-        const newErrors ={};
+        const newErrors = {};
 
         if(!formData.businessName) newErrors.businessName = 'Business Name is required';
         if(!formData.email) newErrors.email = 'Email is required';
-        if(!formData.password) newErrors = 'Password is required';
+        if(!formData.password) newErrors.password = 'Password is required';
         if(formData.password !== formData.confirmPassword)
             newErrors.confirmPassword = 'Passwords do not match';
         if(!formData.businessType) newErrors.businessType = 'Business Type is required';
@@ -32,7 +33,7 @@ const BusinessRegister = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const validationErrors = validate();
-        if(Objects.keys(validationErrors).length > 0){
+        if(Object.keys(validationErrors).length > 0){
             setErrors(validationErrors)
         } else {
             console.log('Form submitted:', formData)
@@ -41,107 +42,122 @@ const BusinessRegister = () => {
     }
 
     return(
-        <div>
-            <h1>Business Registration</h1>
+        <div className="business-reg-page">
+                
+                <div className="business-features">
+                    <h2 className="business-features-heading">Business Features</h2>
 
-            <form onSubmit={handleRegister}>
-                <h2>Add a New Business</h2>
-
-                <div>
-                    <label>Business Name:</label>
-                    <input
-                        type="text"
-                        name="businessName"
-                        value={formData.businessName}
-                        onChange={handleChange}
-                        required
-                        />
-                        {errors.businessName && <p>{errors.businessName}</p>}
+                    <ul className="business-features-listing">
+                        <li>Set up your business profile and highlight key details.</li>
+                        <li>Advertise special offers, discounts, and event to draw in customers.</li>
+                        <li>Monitor customer feedback and track business performance with analytics.</li>
+                    </ul>
                 </div>
 
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="text"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                        />
-                        {errors.email && <p>{errors.email}</p>}
-                </div>
+                <form onSubmit={handleSubmit} className="business-reg-form">
+                <h2 className="business-reg-form-heading">Add a New Business</h2>
 
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="text"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                        />
-                        {errors.password && <p>{errors.password}</p>}
-                </div>
+                <div className="form-row">
+                    <div className="form-group">
+                            <label className="label-group">Business Name:</label>
+                            <input
+                                type="text"
+                                name="businessName"
+                                value={formData.businessName}
+                                onChange={handleChange}
+                                placeholder="Enter business name"
+                                className="form-input"
+                                required
+                                />
+                                {errors.businessName && <p>{errors.businessName}</p>}
+                        </div>
 
-                <div>
-                    <label>Confirm Password:</label>
-                    <input
-                        type="text"
-                        name="confirmPassword"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
-                        required
-                        />
-                        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-                </div>
-
-                <div>
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        name="phone"
-                        value={formData.phone}
-                        onChange={handleChange}
-                        required
-                        />
-                </div>
-
-                <div>
-                    <label>Address:</label>
-                    <input
-                        type="text"
-                        name="address"
-                        value={formData.address}
-                        onChange={handleChange}
-                        required
-                        />
-                </div>
-
-                <div>
-                    <label>Phone Number:</label>
-                    <select
-                        name="businessType"
-                        value={formData.businessType}
-                        onChange={handleChange}
-                        required
-                        >
-
-                        <option value=''>Select</option>
-                        <option value="Ex1">Gym</option>
-                        <option value="Ex2">Fitness Center</option>
-                    </select>
-                    {errors.businessType && <p>{errors.businessType}</p>}
+                        <div className="form-group">
+                            <label className="label-group">Email:</label>
+                            <input
+                                type="text"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                placeholder="Enter Email Address"
+                                className="form-input"
+                                required
+                                />
+                                {errors.email && <p>{errors.email}</p>}
+                        </div>
                 </div>
                 
-                <button type="submit">Add Business</button>
+
+                <div className="form-row">
+                    <div className="form-group">
+                        <label className="label-group">Password:</label>
+                        <input
+                            type="text"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="Enter password"
+                            className="form-input"
+                            required
+                            />
+                            {errors.password && <p>{errors.password}</p>}
+                    </div>
+
+                    <div className="form-group">
+                        <label className="label-group">Confirm Password:</label>
+                        <input
+                            type="text"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            placeholder="Enter confirm password"
+                            className="form-input"
+                            required
+                            />
+                            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                    </div>
+                </div>
+
+
+                <div className="form-row">
+                    <div className="form-group">
+                        <label className="label-group">Address:</label>
+                        <input
+                            type="text"
+                            name="address"
+                            value={formData.address}
+                            onChange={handleChange}
+                            placeholder="Enter street address"
+                            className="form-input"
+                            required
+                            />
+                    </div>
+
+
+                    <div className="form-group">
+                        <label className="label-group">Business Type:</label>
+                        <select
+                            name="businessType"
+                            value={formData.businessType}
+                            onChange={handleChange}
+                            className="form-input-select"
+                            required
+                            >
+
+                            <option value=''>Select</option>
+                            <option value="Ex1">Gym</option>
+                            <option value="Ex2">Fitness Center</option>
+                        </select>
+                        {errors.businessType && <p className="error-message">{errors.businessType}</p>}
+                    </div>
+                </div>
+               
+                <button type="submit" className="submit-button">Add Business</button>
 
             </form>
 
-
         </div>
-
     );
-
 };
 
 export default BusinessRegister;
