63e4d91645ac7338b770796f45741b38592319af
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-19 11:28:18 +0200

    Modular Updates

--- business-discovery/src/App.js
+++ business-discovery/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import Homepage from "./pages/HomePage";
-//import UserRegister from "./pages/user-reg-page";
-//import UserLogin from "./pages/user-login-page";
-//import BusinessRegister from "./pages/business-reg-page";
-import BusinessLogin from "./pages/business-login-page";
+//import Homepage from "./startPages/HomePage";
+//import UserRegister from "./startPages/user-reg-page";
+//import UserLogin from "./startPages/user-login-page";
+//import BusinessRegister from "./startPages/business-reg-page";
+import BusinessLogin from "./startPages/business-login-page";
 
 function App() {
   return (
