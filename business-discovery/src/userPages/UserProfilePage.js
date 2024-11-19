e3bcb644e82f0c15c5e8849de2cbb9ed8071e338
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-19 13:46:59 +0200

    Create UserProfilePage.js

---
+++ business-discovery/src/userPages/UserProfilePage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const UserProfile = () => {
+
+
+    return (
+        <div className='profile-page'>
+            <div className='profile-header'>
+                /**Add Profile Picture here */
+
+                <div className='profile-info'>
+                    /**Add profile info here */
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserProfile;
