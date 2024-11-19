25808b4dd53a8b0b5c19d2a87a898fde34cbcbfe
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-19 12:23:12 +0200

    Create HeroSection.js

---
+++ business-discovery/src/userPages/HeroSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+    return(
+        <div className="heroSection-container">
+            <h2 className="heroSection-text">Discover Local Gems Near You</h2>
+            <button className="explore-button">Explore Now</button>
+        </div>
+    );
+};
+
+export default HeroSection;
