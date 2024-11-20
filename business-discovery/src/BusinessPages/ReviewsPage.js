c1427d7555cc8dcb441b309256664621a2efc75c
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-20 14:06:35 +0200

    Create ReviewsPage.js

---
+++ business-discovery/src/BusinessPages/ReviewsPage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const BusinessReviews = () => {
+    
+    return(
+        <div className='reviews-page'>
+            <h1 className='reviews-page-heading'>Customer Reviews</h1>
+
+            <h2 className='reviews-list-heading'>Reviews</h2>
+
+            <ul className='reviews-list'>
+                (/**Display reviews here */)
+            </ul>
+        </div>
+    );
+};
+
+export default BusinessReviews;
