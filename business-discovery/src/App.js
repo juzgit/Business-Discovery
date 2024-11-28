58ecf6dedb1133bf7b4a3be949a27d943f764bef
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-11-28 14:28:06 +0200

    Added page routing

--- business-discovery/src/App.js
+++ business-discovery/src/App.js
@@ -1,28 +1,53 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import UserNavBar from "./userPages/UserHeader";
-//import HeroSection from "./userPages/HeroSection";
-//import UserHomePage from "./userPages/HomePageUser";
-//import UserDiscoverBusiness from "./userPages/DiscoverBusinessPage";
-//import CategoriesPage from "./userPages/CategoriesPage";
-//import UserProfile from "./userPages/UserProfilePage";
+import { Route, Routes } from "react-router-dom";
 
+//main homepage
+import Homepage from "./startPages/HomePage";
 
-//import BusinessUserHP from "./BusinessPages/BusinessUserHP";
-//import BusinessHeader from "./BusinessPages/BusinessHeader";
-//import BusinessMenuHeader from "./BusinessPages/BusinessMenuHeaderComponent";
-//import BusinessProfile from "./BusinessPages/BusinessProfile";
+//user register and login
+import UserRegister from "./startPages/user-reg-page";
+import UserLogin from "./startPages/user-login-page";
+
+//business user register and login
+import BusinessRegister from "./startPages/business-reg-page";
+import BusinessLogin from "./startPages/business-login-page";
+
+// Normal user end
+import UserHomePage from "./userPages/HomePageUser";
+import UserDiscoverBusiness from "./userPages/DiscoverBusinessPage";
+import CategoriesPage from "./userPages/CategoriesPage";
+import UserProfile from "./userPages/UserProfilePage";
+
+// Businesss User end pages
+import BusinessUserHP from "./BusinessPages/BusinessUserHP";
+import BusinessProfile from "./BusinessPages/BusinessProfile";
 import BusinessPromotions from "./BusinessPages/PromotionsPage";
-//import BusinessReviews from "./BusinessPages/ReviewsPage";
+import BusinessReviews from "./BusinessPages/ReviewsPage";
 
 function App() {
   return (
     <div className="App">
-        <Router>
             <Routes>
-            <Route path="/" element={<BusinessPromotions />} />
+              {/**Starter pages */}
+              <Route path="/" element={<Homepage />} />
+              <Route path="/user-register" element={<UserRegister />} />
+              <Route path="/user-login" element={<UserLogin />} />
+              <Route path="/business-register" element={<BusinessRegister />} />
+              <Route path="/business-login" element={<BusinessLogin />} />
+
+              {/**User end pages */}
+              <Route path="/user-homepage" element={<UserHomePage />} />
+              <Route path="/user-discover" element={<UserDiscoverBusiness />} />
+              <Route path="/user-categories" element={<CategoriesPage />} />
+              <Route path="/user-profile" element={<UserProfile />} />
+
+              {/**Business end pages */}
+              <Route path="/business-dashboard" element={<BusinessUserHP />} />
+              <Route path="/business-profile" element={<BusinessProfile />} />
+              <Route path="/business-profile" element={<BusinessProfile />} />
+              <Route path="/business-promotions" element={<BusinessPromotions />} />
+              <Route path="/business-reviews" element={<BusinessReviews />} />
             </Routes>
-        </Router>
     </div>
   );
 };
