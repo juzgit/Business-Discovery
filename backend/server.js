8471e216fb95ec8ab64b383a23bed9f2cacc65a1
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2024-12-30 14:14:44 +0200

    Added the frontend url of render.com

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotnev.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://business-discovery-frontend.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
